b2e92ebf599f998d87035a49d2ef931a126a44cf
HarmanSingh706 <66528243+HarmanSingh706@users.noreply.github.com>
2021-02-14 21:04:55 -0500

    Add files via upload

---
+++ screens/GetTips.js
@@ -0,0 +1,83 @@
+import React,{Component} from 'react'
+import {View,Text,StyleSheet,FlatList} from 'react-native'
+import MyHeader from '../components/MyHeader'
+import { ListItem } from 'react-native-elements'
+import db from '../config'
+
+export default class GetTips extends Component{
+    constructor(){
+        super()
+        this.state={
+            productName:'',
+            purposeOfProduct:'',
+            contactPhone:'',
+            tipsList:[]
+        }
+        this.requestRef= null
+        }
+        getAddedTipsList =()=>{
+         this.requestRef = db.collection("tips")
+         .onSnapshot((snapshot)=>{
+           var addedTipsList = snapshot.docs.map((doc) => doc.data())
+           this.setState({
+             addedTipsList : addedTipsList
+           });
+         })
+       }
+     
+       componentDidMount(){
+         this.getAddedTipsList()
+       }
+     
+       componentWillUnmount(){
+         this.requestRef();
+       }
+     
+       keyExtractor = (item, index) => index.toString()
+     
+       renderItem = ( {item, i} ) =>{
+         return (
+           <ListItem
+             key={i}
+             title={item.productName}
+             subtitle={item.purposeOfProduct}
+             subtitle={item.contactPhone}
+             titleStyle={{ color: 'black', fontWeight: 'bold' }}
+             bottomDivider
+           />
+         )
+       }
+    render(){
+        return(
+            <View>
+                 <MyHeader title="Cleaning Tips"/>
+                 <View style={{flex:1}}>
+          {
+            this.state.tipsList.length === 0
+            ?(
+              <View style={styles.subContainer}>
+                <Text style={{ fontSize: 20}}>Get Your Tips Here</Text>
+              </View>
+            )
+            :(
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={this.state.getAddedTipsList}
+                renderItem={this.renderItem}
+              />
+            )
+          }
+        </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    subContainer:{
+        flex:1,
+        fontSize: 20,
+        justifyContent:'center',
+        alignItems:'center'
+      }
+})
